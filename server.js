46e7fa4dd665abc73a3213476a36ec39174aae88
Sashankr <sashankr97@gmail.com>
2024-02-19 12:26:53 +0530

    basic route for workout

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const workoutRoutes = require("./src/routes/saveWorkout");
+
+app.use("/api/v1/workout", workoutRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
