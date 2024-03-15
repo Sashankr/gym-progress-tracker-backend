5b068ddd3dd698ff8c8953be9b8eb1a716ee8136
Sashankr <sashankr97@gmail.com>
2024-03-15 08:32:52 +0530

    added login api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -45,6 +45,40 @@ const UserController = {
       });
     }
   },
+  async signin(req, res, next) {
+    try {
+      const { emailId, password } = req.body;
+      if (!(emailId && password)) {
+        return res.status(400).send({
+          message: "All fields are required",
+        });
+      }
+      const user = await UserModel.findOne({ emailId });
+      const checkIfPasswordSame = await bcrypt.compare(password, user.password);
+
+      if (user && checkIfPasswordSame) {
+        const token = JWT.sign(
+          { user_id: user._id, emailId },
+          process.env.SECRET_KEY,
+          { expiresIn: "5h" }
+        );
+        user.token = token;
+        return res.status(200).send({
+          message: "Login Verified",
+          status: 200,
+          data: user,
+        });
+      } else {
+        return res.send(400).send({
+          message: "Invalid Credentials",
+        });
+      }
+    } catch (err) {
+      res.status(500).send({
+        message: "Something went wrong",
+      });
+    }
+  },
 };
 
 module.exports = UserController;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", UserController.signup);
+router.post("/login", UserController.signin);
 
 module.exports = router;
