c95b45d8931106462f781750ca2cbf718515354c
Sashankr <sashankr97@gmail.com>
2024-03-15 08:06:42 +0530

    updated signup api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,50 @@
 const UserModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
 const UserController = {
-  async signup(req, res, next) {},
+  async signup(req, res, next) {
+    try {
+      const { fullName, username, emailId, password } = req.body;
+      console.log("body", req.body);
+      if (!(fullName && username && emailId && password)) {
+        return res.send(400, {
+          message: "All fields are required",
+        });
+      }
+      const oldUser = await UserModel.findOne({ emailId });
+      if (oldUser) {
+        return res.send(409, {
+          message: "This user already exists, please login",
+        });
+      }
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      console.log(encryptedPassword);
+      const user = await UserModel.create({
+        fullName,
+        username,
+        emailId: emailId.toLowerCase(),
+        password: encryptedPassword,
+      });
+
+      const token = JWT.sign(
+        { user_id: user._id, emailId, username },
+        process.env.SECRET_KEY,
+        { expiresIn: "5h" }
+      );
+      user.token = token;
+      return res.send(201, {
+        message: "Signup Successfull!",
+        status: 201,
+        data: user,
+      });
+    } catch (err) {
+      console.log(err);
+      res.send(500, {
+        message: "Something went wrong",
+      });
+    }
+  },
 };
 
 module.exports = UserController;
