ba5f4290191739970e9c91a7660e3d3c2e7f6a9d
Sashankr <sashankr97@gmail.com>
2024-03-22 07:30:56 +0530

    updated cors policy

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
 const cors = require("cors");
 const { connectToDb } = require("./src/config/database");
+const helmet = require("helmet");
 connectToDb();
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+      allowedOrigins: [
+        "https://gym-progress-tracker.vercel.app",
+        "http://localhost:3000",
+      ],
+    },
+  })
+);
+
 const corsOptions = {
   origin: ["http://localhost:3000", "https://gym-progress-tracker.vercel.app"],
   optionsSuccessStatus: 200, // for some legacy browsers
 };
+
 app.options("*", cors(corsOptions));
 
 app.use(cors(corsOptions));
@@ -23,6 +37,7 @@ const authRoutes = require("./src/routes/auth");
 app.use("/api/v1/workout", workoutRoutes);
 app.use("/api/v1/user", authRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, (data) => {
+  console.log("data", data);
   console.log(`Server running on port ${PORT}`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,6 @@ const { MONGO_DB_URL, MONGO_DB_USERNAME, MONGO_DB_PASSWORD } = process.env;
 exports.connectToDb = () => {
   // Construct the MongoDB connection URL with credentials
   const connectionString = `mongodb://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_URL}`;
-  console.log(connectionString);
   // Connecting to the database with authentication
   mongoose
     .connect(connectionString, {
